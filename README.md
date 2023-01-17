a90891e84320ddbc1fd1a728f2e2757b2a6ea0dd
CarlosHugoRodriguezC <cahuroca.aw@gmail.com>
2023-01-17 00:43:06 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# This is a personal practice projects
+
+I made this project as practice for learning new technologies like frameworks, concepts or just new things that I interest me, I'll still adding new technologies as I discover them .
+
+## Contains
+
+- Front-End projects
+- Back-End projects
+
+## Technologies
+
+- Nest JS
+- Graphql
+- Postgresql
+- Nextjs
+- Typescript
+- Tailwind, headless ui, mui, picocss and others.
+
---
+++ shop-gql/src/app.module.ts
@@ -0,0 +1,34 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { debug } from 'console';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
