b64251344aca1e776bfcaadeb344b62555c76073
Carlos Hugo Rodriguez Calderon <cahuroca.aw@gmail.com>
2023-02-18 02:53:24 -0600

    next auth base ended

--- comanda-api/src/auth/auth.controller.ts
+++ comanda-api/src/auth/auth.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body} from '@nestjs/common';
 import { CreateUserInput } from '../users/dto/create-user.input';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto';
--- comanda-front/src/pages/api/auth/[...nextauth].ts
+++ comanda-front/src/pages/api/auth/[...nextauth].ts
@@ -4,12 +4,55 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions: AuthOptions = {
   providers: [
-    CredentialsProvider(
-        { 
-            name: 'Credentials'
+    CredentialsProvider({
+      name: 'Login',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: '*******',
+        },
+      },
+      async authorize(credentials) {
+        return await new Promise(() => true);
+      },
+    }),
+  ],
+  jwt: {},
+  session: {
+    maxAge: 2592000,
+    strategy: 'jwt',
+    updateAge: 86400,
+  },
+  pages: {
+    signIn: '/auth/login',
+    newUser: '/auth/register',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        switch (account.type) {
+          case 'oauth':
+            token.user = null; // TODO: implement the endpoint
+            break;
+          case 'credentials':
+            token.user = user;
         }
-        )
-    ],
+      }
+      return token;
+    },
+    async session({session, token, user}) {
+        session.accessToken = token.accessToken as string;
+        session.user = token.user as any
+        return session;
+    }
+  },
 };
 
 export default NextAuth(authOptions);
